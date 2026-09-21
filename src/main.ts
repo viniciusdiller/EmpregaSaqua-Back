3a387320360111f444a66b0609a19e1648038f5f
Vinícius Diller <viniciusdiller@hotmail.com>
2026-09-21 14:49:28 -0300

    feat(api): configure Swagger documentation and improve startup logs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter.js';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,7 +29,20 @@ async function bootstrap() {
       transform: true, // Automatically transforms payloads to be objects typed according to their DTO classes
     }),
   );
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('EmpregaSaqua API')
+    .setDescription('The EmpregaSaqua API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`\n🚀 API rodando em http://localhost:${port}`);
+  console.log(`📖 Swagger docs disponível em http://localhost:${port}/api/docs (Use Ctrl + Click para abrir)\n`);
 }
 await bootstrap();
